63b4f6c4583a506284424d32164c436054149b32
MrSilent2001 <bandarigodadm.21@uom.lk>
2024-11-29 16:37:03 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Button } from "@/components/ui/button"
+import './App.css'
+
+function App() {
+
+  return (
+      <>
+          <h1 className="text-3xl font-bold underline text-red-700">
+              Hello world!
+          </h1>
+          <Button>Click me</Button>
+      </>
+  )
+}
+
+export default App
